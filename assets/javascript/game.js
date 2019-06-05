a8cfc28fa9ec4c439d395d0868591a7d0be6f084
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-04 19:34:49 -0700

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,12 @@ start game (run js code)
 start word bank (do not show word bank)
 computer choose a word from word bank
 display each character as '_'
+    --for loop
+    --document.write("_")
 user press letter key
+    --onkeypress
+    --set in array
+    --display wrong key
 if right, reveal letters
 if wrong, user lose a guess and show letter guessed
     --use counter-- to decrease guess 
@@ -21,6 +26,10 @@ count wins and loss
 */
 
 const bank = ['apple', 'princess', 'charming'];
+let wrong = [];
+let guess = 10;
+let win = 0;
+let loss = 0;
 
 function random_word(bank)
 {
@@ -30,10 +39,23 @@ return bank[Math.floor(Math.random()*bank.length)];
      
 }
 
-document.write(random_word(bank));
 
 
-console.log(random_word(bank));
+for(let i = 0; i < random_word(bank).length; i++) {
+    document.write("_ ");
+    if (onkeypress == random_word(bank[i])) {
+        
+    }
+    else {
+        document.wrinte(onkeypress === wrong);
+    }
+}
+
+
+
+
+
+
 
 
 /*textForm.addEventListener('submit', function(event) {
