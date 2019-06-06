98ab9dba47545562704516dbba742b18815e7aec
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-06 14:44:11 -0700

    html css js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,71 @@
+//start game (run js code)
+
+$(document).ready(function() {
+    
+//start word bank (do not show word bank)
+const bank = ['apple', 'princess', 'charming'];
+let wrong = "";
+let guess = 12;
+let win = 0;
+let loss = 0;
+
+//computer choose a word from word bank
+$("#word").text(function random_word(bank)
+{
+  
+return bank[Math.floor(Math.random()*bank.length)];
+
+     
+})
+
+$(document).keypress(function(event){ 
+      
+    let key = (event.keyCode ? event.keyCode : event.which); 
+    let ch=String.fromCharCode(key) 
+        console.log(ch);  
+        $("#word").text($(key).text());
+        
+      
+  
+});
+
+//display each character as '_'
+
+//for(let i = 0; i < random_word(bank).length; i++) {
+  // $("#word").text("_ ");
+
+//}
+
 /*
-start game (run js code)
-start word bank (do not show word bank)
-computer choose a word from word bank
-display each character as '_'
-    --for loop
-    --document.write("_")
 user press letter key
     --onkeypress
     --set in array
     --display wrong key
 if right, reveal letters
 if wrong, user lose a guess and show letter guessed
+    guess--
     --use counter-- to decrease guess 
     --guess number starts at 12 counter = 12
     --can't re-guess the same letters
 if user guess all correct letters, display win and correct word
-    --alert "You guessed it!"
+    --alert("You guessed it!")
     --win ++
 if user runs out of guesses, display loss
-    --alert "You ran out of guesses." + "Better luck next time!"
+    --alert("You ran out of guesses." + "Better luck next time!")
     --loss ++
 coputer choose another word - regardless of win/loss
 count wins and loss
 
 */
 
-const bank = ['apple', 'princess', 'charming'];
-let wrong = [];
-let guess = 12;
-let win = 0;
-let loss = 0;
-
-function random_word(bank)
-{
-  
-return bank[Math.floor(Math.random()*bank.length)];
-
-     
-}
-
-
-for(let i = 0; i < random_word(bank).length; i++) {
-    document.write("_ ");
-   // if (onkeypress == random_word(bank[i])) {
-        
-    }
-    //else {
-      //  document.write(onkeypress);
-   // }
-//}
 
 
 
 
+//$("#word").prepend("<br><hr>" + wrong);
 
 
-
-
+})
 
 
 /*textForm.addEventListener('submit', function(event) {
